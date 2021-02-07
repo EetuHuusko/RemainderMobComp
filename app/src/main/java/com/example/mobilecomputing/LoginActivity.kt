a4eaeab194fc03b6fb710bc4da728b195120537d
EetuHuusko <eetuhuusko@gmail.com>
2021-02-07 14:41:57 +0200

    Modified Login screen, main activity screen and added fingerprint login

---
+++ app/src/main/java/com/example/mobilecomputing/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mobilecomputing
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.Context
+
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
